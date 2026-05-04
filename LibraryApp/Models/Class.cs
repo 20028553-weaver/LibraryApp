dd08e69549d1b386a5f151593099e0f55fca8ee9
20028553-weaver <20028553@students.koi.edu.au>
2026-05-05 08:48:55 +1000

    Add project files.

---
+++ LibraryApp/Models/Class.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = "";
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ LibraryApp/Program.cs
@@ -0,0 +1,28 @@
+using LibraryApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
